89151aa65fdf9d7fc7f02a9b6dde6caf8ef9175a
Maksatai <erkbvna@gmail.com>
2021-01-22 20:00:31 +0600

    added books

--- main/models.py
+++ main/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Books(models.Model):
     title=models.CharField(max_length=100)
     subtitle=models.CharField(max_length=200)
-    description=models.CharField(max_length=1000)
-    price=models.IntegerField
+    description=models.TextField()
+    price=models.CharField(max_length=50)
     genre=models.CharField(max_length=50)
     author=models.CharField(max_length=100)
-    year=models.IntegerField
-    date=models.DateField(auto_now_add=False)
+    year=models.CharField(max_length=10)
+    date=models.CharField(max_length=10)
 
--- main/views.py
+++ main/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render,HttpResponse
 from .models import Books
 
 def homepage(request):
-    return render(request, 'books.html')
-    
+    todo_list=Books.objects.all()
+    return render(request, 'books.html',{"todo_list":todo_list})
+       
+
