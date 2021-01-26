711b4330b08b7d336a06d0f2bd8ac22aa7769085
Maksatai <erkbvna@gmail.com>
2021-01-26 12:24:26 +0600

    added delete function

--- main/views.py
+++ main/views.py
@@ -18,3 +18,9 @@ def add_todo(request):
     todo=Books(title=text,subtitle=text1,description=text2,price=text3,genre=text4,author=text5,year=text6)
     todo.save()
     return redirect(homepage)
+
+def delete_todo(request,id):
+    todo=Books.objects.get(id=id)
+    todo.delete()
+    return redirect(homepage)
+
